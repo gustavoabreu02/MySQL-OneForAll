7c5b296a2bbbb4659112b665bba5c812b1b7e9b5
Gustavo Abreu <gustavofart371@gmail.com>
2022-09-14 03:18:43 -0300

    req4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT us.nome_usuario AS usuario,
+IF (YEAR(MAX(re.reproducao_data)) >= 2021, 'Usuário ativo', 'Usuário inativo') AS status_usuario
+FROM SpotifyClone.usuarios AS us
+INNER JOIN SpotifyClone.reproducao AS re
+ON re.id_usuario = us.id_usuario
+GROUP BY us.nome_usuario
+ORDER BY us.nome_usuario ASC;
