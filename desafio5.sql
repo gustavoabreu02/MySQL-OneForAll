d2ab9408a5f5ebe40dd1c05121cf3e5916333598
Gustavo Abreu <gustavofart371@gmail.com>
2022-09-14 03:24:18 -0300

    req5

---
+++ desafio5.sql
@@ -0,0 +1,8 @@
+SELECT ca.nome_cancao AS cancao,
+COUNT(re.id_cancao) AS reproducoes
+FROM SpotifyClone.cancoes AS ca
+INNER JOIN SpotifyClone.reproducao AS re
+ON ca.id_cancao = re.id_cancao
+GROUP BY ca.nome_cancao
+ORDER BY reproducoes DESC,
+ca.nome_cancao LIMIT 2;
