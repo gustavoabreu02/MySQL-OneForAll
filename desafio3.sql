cf68fa666fbefb465f712afb90f358b1336f66cb
Gustavo Abreu <gustavofart371@gmail.com>
2022-09-14 03:13:16 -0300

    req3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT us.nome_usuario AS 'usuario',
+ COUNT(rep.id_cancao) as 'qt_de_musicas_ouvidas',
+ ROUND(SUM(ca.duracao_segundos / 60), 2) AS 'total_minutos'
+FROM SpotifyClone.reproducao as rep
+INNER JOIN SpotifyClone.usuarios as us
+ON us.id_usuario = rep.id_usuario
+INNER JOIN SpotifyClone.cancoes as ca
+ON ca.id_cancao = rep.id_cancao
+GROUP BY us.nome_usuario
+ORDER BY us.nome_usuario ASC;
